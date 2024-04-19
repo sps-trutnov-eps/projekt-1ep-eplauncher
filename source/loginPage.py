1d6c57a0507524cd41ab5b8e03e1753e4e0c20af
Matěj Kubíček <kubicekm23@spstrutnov.cz>
2024-04-19 19:12:10 +0200

    Oprava login stránky

--- source/loginPage.py
+++ source/loginPage.py
@@ -19,7 +19,6 @@ loginText = "login"
 loginTextSurface = font.render(loginText, True, white)
 
 
-
 def login(rozliseni, window, clock):
     running = True
     clock.tick(60)
@@ -42,68 +41,11 @@ def login(rozliseni, window, clock):
         window.blit(jmenoSurface, (rozliseni[0]/2 - 100, 480))
         window.blit(hesloSurface, (rozliseni[0]/2 - 100, 550))
         window.blit(loginTextSurface, (rozliseni[0] / 2 - 100, 645))
-        #Human input
-
-def Prihlaseni():
-    font = pygame.font.Font(None, 32)
-    clock = pygame.time.Clock()
-    input_box = pygame.Rect(100, 100, 140, 32)
-    color_inactive = pygame.Color('lightskyblue3')
-    color_active = pygame.Color('dodgerblue2')
-    color = color_inactive
-    active = False
-    text = ''
-    done = False
-
-    while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                # If the user clicked on the input_box rect.
-                if input_box.collidepoint(event.pos):
-                    # Toggle the active variable.
-                    active = not active
-                else:
-                    active = False
-                # Change the current color of the input box.
-                color = color_active if active else color_inactive
-            if event.type == pygame.KEYDOWN:
-                if active:
-                    if event.key == pygame.K_RETURN:
-                        print(text)
-                        text = ''
-                    elif event.key == pygame.K_BACKSPACE:
-                        text = text[:-1]
-                    else:
-                        text += event.unicode
-
-        window.fill((30, 30, 30))
-        # Render the current text.
-        txt_surface = font.render(text, True, color)
-        # Resize the box if the text is too long.
-        width = max(200, txt_surface.get_width()+10)
-        input_box.w = width
-        # Blit the text.
-        window.blit(txt_surface, (input_box.x+5, input_box.y+5))
-        # Blit the input_box rect.
-        pygame.draw.rect(window, color, input_box, 2)
-
-        window.fill((30, 30, 30))
-        # Render the current text.
-        txt_surface = font.render(text, True, color)
-        # Blit the text.
-        window.blit(txt_surface, (input_box.x+5, input_box.y+5))
-        # Blit the input_box rect.
-        pygame.draw.rect(window, color, input_box, 2)
-
-        
+        pygame.display.flip()
 
         pos = pygame.mouse.get_pos()
         if Login.colliderect((pos[0], pos[1], 1, 1)) and pygame.mouse.get_pressed()[0]:
-            completed_login = False
+            completed_login = True
             running = False
 
-
-    completed_login = True
     return completed_login
