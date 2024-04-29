8f153ea98ee37e341e03a7da76d3fa92babcd56b
Honza Serbousek <serbousekj23@spstrutnov.cz>
2024-04-29 14:09:57 +0200

    priprava 2

--- source/minihry/Obrana T10/Boj o T10.py
+++ source/minihry/Obrana T10/Boj o T10.py
@@ -1,1 +1,22 @@
+import sys
 import pygame
+pygame.init()
+
+rozliseni_okna = (1320, 768)
+okno = pygame.display.set_mode(rozliseni_okna)
+#----------------------------------------------------------------------
+while True:
+    for udalost in pygame.event.get():
+        if udalost.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+      
+    okno.fill(143, 64, 32)
+      
+#----------------------------------------------------------------------
+            
+            
+            
+            
+            
+    pygame.display.flip()
