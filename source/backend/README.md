30638faf75a6fdd8a941dfc6daa1333cbe718551
Jakub Šenkýř <senkyr@spstrutnov.cz>
2024-05-03 11:12:26 +0200

    Export SQL z databáze

--- source/backend/README.md
+++ source/backend/README.md
@@ -4,6 +4,8 @@ Složka `api/` obsahuje PHP soubory s jednotlivými skripty, které je možné v
 
 Soubor `index.html` slouží k ručnímu testování jednotlivých skriptů.
 
+Složka `sql/` obsahuje SQL soubory, kterými lze vytvořit příslušné tabulky a naplnit je ukázkovými daty.
+
 ## Seznam skriptů
 
 * `hello.php`
---
+++ source/backend/sql/1ep_eplauncher_ownership.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Počítač: localhost
+-- Vytvořeno: Pát 03. kvě 2024, 11:08
+-- Verze serveru: 10.1.48-MariaDB-0+deb9u2
+-- Verze PHP: 7.3.33-1+0~20211119.91+debian9~1.gbp618351
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Databáze: `ucit_senkyr`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `1ep_eplauncher_ownership`
+--
+
+CREATE TABLE `1ep_eplauncher_ownership` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `game_id` int(11) NOT NULL,
+  `from_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+--
+-- Vypisuji data pro tabulku `1ep_eplauncher_ownership`
+--
+
+INSERT INTO `1ep_eplauncher_ownership` (`id`, `user_id`, `game_id`, `from_date`) VALUES
+(1, 1, 1, '2024-05-03 09:08:13'),
+(2, 2, 2, '2024-05-03 09:08:13'),
+(3, 1, 2, '2024-05-03 09:08:20');
+
+--
+-- Indexy pro exportované tabulky
+--
+
+--
+-- Indexy pro tabulku `1ep_eplauncher_ownership`
+--
+ALTER TABLE `1ep_eplauncher_ownership`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pro tabulky
+--
+
+--
+-- AUTO_INCREMENT pro tabulku `1ep_eplauncher_ownership`
+--
+ALTER TABLE `1ep_eplauncher_ownership`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
