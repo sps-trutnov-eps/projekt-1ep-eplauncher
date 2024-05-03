29240f463b6e48bde619ab06442b0be9281fbcef
Ondřej Imlauf <imlaufo23@spstrutnov.cz>
2024-05-03 12:43:55 +0200

    textury pro hru

--- source/minihry/bageta/main.py
+++ source/minihry/bageta/main.py
@@ -4,9 +4,9 @@ import pygame
 pygame.init()
 
 okno = pygame.display.set_mode((1400, 800))
-
-
-
+obrazek_automat = pygame.image.load("automat.png")
+obrazek_automat_vklad = pygame.image.load("automat_vklad.png")
+obrazek_10_korun = pygame.image.load("10_korun.png")
 
 
 
@@ -26,5 +26,8 @@ while True:
     
     
     
+    okno.fill((40,100,200))
+    okno.blit(obrazek_automat_vklad, (0,0))
+    okno.blit(obrazek_10_korun, (774,150))
     pygame.display.flip()
     
