01f36a73e8af681e39a940785ec02b30728468bc
Ondřej Imlauf <imlaufo23@spstrutnov.cz>
2024-05-03 11:45:09 +0200

    založení main

---
+++ source/minihry/Bageta quest/main.py
@@ -0,0 +1,30 @@
+import random
+import sys
+import pygame
+pygame.init()
+
+okno = pygame.display.set_mode((1400, 800))
+
+
+
+
+
+
+
+while True:
+    
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
+            pygame.quit() 
+            sys.exit()    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    pygame.display.flip()
+    
