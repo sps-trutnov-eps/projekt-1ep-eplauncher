e19c0e55b31ab6a8d38cee0e68d52cefdf87820b
Jakub Šenkýř <senkyr@spstrutnov.cz>
2024-05-13 13:57:14 +0200

    Skript v Pythonu demonstrující použití API

---
+++ source/backend/demo.py
@@ -0,0 +1,37 @@
+# Ukazka pouziti serveroveho API primo z Pythonu
+# (c) Jakub Senkyr, 2024
+
+import requests
+import json
+
+print('Tento skript demonstruje komunikaci s databází na serveru pomocí HTTP')
+input('<Enter> pro pokračování...')
+print()
+
+# 1) hello (prijata data jako JSON objekt)
+url = 'http://senkyr.epsilon.spstrutnov.cz/eplauncher/api/hello.php'
+response = requests.get(url)
+print('Toto jsou surová data vrácená z URL', url)
+print(response.text)
+print()
+
+print('Toto jsou data extrahovaná z formátu JSON')
+print(json.loads(response.text)['message'])
+input('<Enter> pro pokračování...')
+print()
+
+# 2) users (prijata data jako JSON pole objektu)
+url = 'http://senkyr.epsilon.spstrutnov.cz/eplauncher/api/users.php'
+response = requests.get('http://senkyr.epsilon.spstrutnov.cz/eplauncher/api/users.php')
+print('Toto jsou surová data vrácená z URL', url)
+print(response.text)
+print()
+
+print('Toto jsou data extrahovaná z formátu JSON')
+users = json.loads(response.text)
+for user in users:
+    print(user)
+input('<Enter> pro pokračování...')
+print()
+
+print('A to je vše, lidičky!')
