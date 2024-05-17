0a3e6735224e0fa2f84a7928702ac7eb928ddb50
Martin Hoffmann <hoffmannm23@spstrutnov.cz>
2024-05-17 08:34:21 +0200

    Skok, gravitace a další věci
    Skok, gravitace, fps, nové textury

--- source/minihry/hoffmann/main.py
+++ source/minihry/hoffmann/main.py
@@ -5,6 +5,10 @@ pygame.init()
 
 rozliseni_okna = (1280, 900)
 okno = pygame.display.set_mode(rozliseni_okna)
+pygame.display.set_caption("Skákající hráč")
+
+
+barva_pozadi = (50, 50, 50)
 
 class Objekty:
     def __init__(self, x_pos, y_pos, x_velikost, y_velikost, rychlost):
@@ -13,40 +17,77 @@ class Objekty:
         self.x_velikost = x_velikost
         self.y_velikost = y_velikost
         self.rychlost = rychlost
-        
+
     def posun(self):
         self.y_pos += self.rychlost
 
+
 kyselina_textura = pygame.image.load('kyselina.png')
 man_textura = pygame.image.load("normal_man.png")
+man_textura_skok = pygame.image.load("jumping_man.png")
 
-hrac = Objekty (1200, 700, 50, 50, 5)
-koule = Objekty (1000, 500, 50, 50, 4)
-kyselina = Objekty (900, 600, 50, 50, 4)
 
+hrac = Objekty(75, 700, 50, 50, 5)
+koule = Objekty(1000, 500, 50, 50, 4)
+kyselina = Objekty(900, 600, 50, 50, 4)
+hracskok = Objekty(1100, 700, 50, 50, 4)
+
+
+je_na_zemi = True
+rychlost_skok = 20
+gravitace = 0.9
+rychlost_y = 0
+
+
+clock = pygame.time.Clock()
+fps = 60
 
 while True:
     for udalost in pygame.event.get():
         if udalost.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    
+
     stisknute_klavesy = pygame.key.get_pressed()
-    
+
+
     if stisknute_klavesy[pygame.K_LEFT]:
         hrac.x_pos -= hrac.rychlost
-        
+
     if stisknute_klavesy[pygame.K_RIGHT]:
         hrac.x_pos += hrac.rychlost
-         
-    if stisknute_klavesy[pygame.K_SPACE]:
-        hrac.y_pos += 20 - 20
 
-    okno.fill((50, 50, 50))
-    
-    
-    okno.blit(man_textura, (hrac.x_pos, hrac.y_pos))
+
+    if stisknute_klavesy[pygame.K_SPACE] and je_na_zemi:
+        je_na_zemi = False
+        rychlost_y = -rychlost_skok
+
+
+    if not je_na_zemi:
+        hrac.y_pos += rychlost_y
+        rychlost_y += gravitace
+
+
+        if hrac.y_pos >= 700:
+            hrac.y_pos = 700
+            je_na_zemi = True
+            rychlost_y = 0
+
+
+    okno.fill(barva_pozadi)
+
+
+    if not je_na_zemi:
+        okno.blit(man_textura_skok, (hrac.x_pos, hrac.y_pos))
+    else:
+        okno.blit(man_textura, (hrac.x_pos, hrac.y_pos))
+
     pygame.draw.rect(okno, (100, 255, 0), (koule.x_pos, koule.y_pos, koule.x_velikost, koule.y_velikost))
     okno.blit(kyselina_textura, (kyselina.x_pos, kyselina.y_pos))
-    
+    okno.blit(man_textura_skok, (hracskok.x_pos, hracskok.y_pos))
+
     pygame.display.update()
+
+
+    clock.tick(fps)
+
