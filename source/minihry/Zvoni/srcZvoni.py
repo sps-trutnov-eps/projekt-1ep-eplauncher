2f177964b551657203a41120e2b4025a0bdf9d33
Vojtěch Jiruš <jirusv23@spstrutnov.cz>
2024-05-27 14:16:28 +0200

    Dokončení minihry Zvoní

--- source/minihry/Zvoni/srcZvoni.py
+++ source/minihry/Zvoni/srcZvoni.py
@@ -12,12 +12,19 @@ def main():
     global rychlostPrekazky, offsetDvery, dvere, listPrekazek, pozice, WinningPrekazek, pocetSmazanychPrekazek, pozice1X, pozice1Y, okno, goodEngingCheck, possiblePossition, gameOver
 
 
-    greyRectangle = pygame.image.load("Img/ucitel.png")
+    greyRectangle = pygame.image.load("source/minihry/Zvoni/Img/ucitel.png")
     greyRectangle = pygame.transform.scale(greyRectangle, (80,80))
-    dvere = pygame.image.load("Img/dvere.png")
+    dvere = pygame.image.load("source/minihry/Zvoni/Img/dvere.png")
     lineDark = (247, 159, 0)
+    
+    student1 = pygame.image.load("source/minihry/Zvoni/Img/student.png")
+    student1 = pygame.transform.scale(student1, (80, 80))
+    student2 = pygame.image.load("source/minihry/Zvoni/Img/student2.png")
+    student2 = pygame.transform.scale(student2, (80, 80))
+    student3 = pygame.image.load("source/minihry/Zvoni/Img/student3.png")
+    student3 = pygame.transform.scale(student3, (80, 80))
 
-    FPS = 60
+    FPS = 120
 
     pozice1X = 75
     pozice1Y = 300-32
@@ -25,22 +32,26 @@ def main():
     pozice = 2
 
     canMove = True
-    movementTimer = 10 #frames
+    movementTimer = 20 #frames
 
-    rychlostPrekazky = 10
+    rychlostPrekazky = 5
 
     pocetSmazanychPrekazek = 0
-    WinningPrekazek = 100
+    WinningPrekazek = 15
     xLine = 1200  
     offsetDvery = 0
     goodEngingCheck = 0
     listPrekazek = []
+    
+    ZvoniAchivement = False
     
     class Prekazky:
         global pozice
         def __init__(self):
             self.pozice = random.randint(0,3)
             self.poziceX = 1400
+            
+            self.textura = random.randint(1,3)
             
             
         def PohybPrekazky(self):
@@ -51,8 +62,15 @@ def main():
             global pocetSmazanychPrekazek
             if pocetSmazanychPrekazek <= WinningPrekazek:
                 prekazkaRect = pygame.Rect(self.poziceX, ((200*self.pozice)+64), 64, 64)
-                pygame.draw.rect(okno, (185,122,86), prekazkaRect)
-            
+                #pygame.draw.rect(okno, (185,122,86), prekazkaRect)
+
+                if self.textura == 1:
+                    okno.blit(student1, (prekazkaRect[0], prekazkaRect[1]))
+                elif self.textura == 2:
+                    okno.blit(student2, (prekazkaRect[0], prekazkaRect[1]))
+                elif self.textura == 3:
+                    okno.blit(student3, (prekazkaRect[0], prekazkaRect[1]))
+
         def outOfBounds(self):
             global pocetSmazanychPrekazek
             if self.poziceX < -64:
@@ -93,19 +111,16 @@ def main():
         okno.blit(dvere, (1200 - offsetDvery,0))
         
         if pozice1Y <= 350:
-            pozice1Y += 0.5
+            pozice1Y += 0.25
             
         if pozice1X <= 850:
-            pozice1X += 1
+            pozice1X += 0.5
         else:
             goodEngingCheck = 1
 
         possiblePossition = [(75, 100-32), (pozice1X, pozice1Y), (75, 500-32), (75, 700-32)]
 
-            
 
-
-    
     SpawnPrekazek()
     run = True
     while run:
@@ -120,11 +135,11 @@ def main():
 
         okno.fill((250, 206, 112))
 
-        if stisknuteKlavesy[pygame.K_UP] and canMove == True:
+        if stisknuteKlavesy[pygame.K_UP] or stisknuteKlavesy[pygame.K_w] and canMove == True:
             pozice -= 1
             canMove = False
 
-        elif stisknuteKlavesy[pygame.K_DOWN] and canMove == True:
+        elif stisknuteKlavesy[pygame.K_DOWN] or stisknuteKlavesy[pygame.K_s] and canMove == True: 
             pozice += 1
             canMove = False
 
@@ -141,7 +156,7 @@ def main():
             movementTimer -= 1 
             if movementTimer < 0:
                 canMove = True
-                movementTimer = 10
+                movementTimer = 20
                 
         if pocetSmazanychPrekazek >= WinningPrekazek:
             canMove = False
@@ -155,13 +170,20 @@ def main():
         for i in listPrekazek:
             if gameOver == False and pocetSmazanychPrekazek <= WinningPrekazek:
                 i.KontrolaKolize()
+                i.outOfBounds()
                 i.vykresleniPrekazky()
                 i.PohybPrekazky()
-                i.outOfBounds()
             if gameOver == True:
                 
                 gameOverFont = FontChoossenSize("Verdana", 50).render("Narazil jsi do studenta!", True, (220,20,20))
-                okno.blit(gameOverFont, (450, 350))
+                okno.blit(gameOverFont, (300, 350))
+
+                #any key pressed
+                if event.type == pygame.KEYDOWN:
+                    gameOver = False
+                    pozice = 2
+                    listPrekazek.clear()
+                    pocetSmazanychPrekazek = 1
                 
             if pocetSmazanychPrekazek >= WinningPrekazek:
                 if xLine >= 0:
@@ -178,9 +200,13 @@ def main():
         if goodEngingCheck == 1:
             goodEndingFont = FontChoossenSize("Verdana", 50).render("Dostal jsi se do třídy na čas!", True, (220,20,20))
             okno.blit(goodEndingFont, (250, 350))
+            ZvoniAchivement = True
 
         pygame.display.update()
         pygame.time.Clock().tick(FPS) #framerate, FPS
+
+    if ZvoniAchivement == True:
+        return True, "Zvoni"
         
 if __name__ == "__main__":
     main()
